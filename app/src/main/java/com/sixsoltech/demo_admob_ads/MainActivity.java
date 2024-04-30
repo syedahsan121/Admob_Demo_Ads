ea8b7b937a01a9510d090a4280c5f23cb11d007a
Syed Ahsan Ali Shah <creationsdragon@gmail.com>
2024-04-30 13:05:55 +0500

    iam going to implement ads
    version1

---
+++ app/src/main/java/com/sixsoltech/demo_admob_ads/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sixsoltech.demo_admob_ads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
